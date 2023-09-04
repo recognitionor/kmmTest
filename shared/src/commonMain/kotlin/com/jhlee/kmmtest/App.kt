29d83776c1ef987b22936b2ba18991f4d3bf204f
junghee <junghee@nhn.com>
2023-09-04 14:29:21 +0900

    android shared compose ui set

---
+++ shared/src/commonMain/kotlin/com/jhlee/kmmtest/App.kt
@@ -0,0 +1,12 @@
+package com.jhlee.kmmtest
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun App() {
+    Column {
+        Text(text = "text")
+    }
+}
